4b6136aee7b876574a001e70ab661c5e6158bdbd
Riley Burke <rileyburke0@hotmail.com>
2022-05-15 20:50:29 -0230

    All JSON data displayed on page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import logo from './logo.svg';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import { MovieReviews, SubmitReview } from "./pages";
 
+
+
 function App() {
+  const [movies, setMovies] = useState(null);
+
+  useEffect( () => {
+    fetch("./movies.json")
+    .then( response => response.json() )
+    .then( setMovies )
+    .then( console.log(movies) )
+    .catch( e => console.log(e.message));
+  }, []);
+
   return (
     <div>
-      <h1>APP</h1>
       <Routes>
-        <Route path="/" element={<MovieReviews />} />
+        <Route path="/" element={<MovieReviews moviesList={movies} setMovies={setMovies} />} />
         <Route path="/submit" element={<SubmitReview />} />
       </Routes>
     </div>
--- src/pages.js
+++ src/pages.js
@@ -1,11 +1,31 @@
 import React from "react";
 import Link from "react-router-dom";
 
-export function MovieReviews() {
+export function MovieReviews( { moviesList = [], onChangeMovies = f => f }) {
+    if( moviesList == null || moviesList == undefined ) return <h2></h2>
+
     return(
-        <div>
+        <>
             <h1>Movie Reviews</h1>
-        </div>
+            {console.log(moviesList)}
+            {
+            moviesList.map ( (movie) => {
+                return(
+                <>
+                    <h2>{movie.name}</h2>
+                    <img src={movie.poster} />
+                    <h4>Release date: {movie.releaseDate}</h4>
+                    <h4>Starring:</h4>
+                    <ul>
+                        {movie.actors.map( (actor) => {
+                            return <li>{actor}</li>
+                        })}
+                    </ul>
+                    <h4>Rating: {movie.rating}</h4>
+                </>)
+            })
+           }
+        </>
     );
 }
 
@@ -13,10 +33,10 @@ export function SubmitReview() {
     return(
         <div>
             <h1>Submit a Movie Review</h1>
-            {/* <form>
+            <form>
                 <label for="movie_name">Movie Title: </label>
                 <input type="text" id="movie_name" name="movie_name "/>
-            </form> */}
+            </form>
         </div>
     );
 }
